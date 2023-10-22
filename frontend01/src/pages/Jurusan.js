5eb73c38aa3d49846dfaf275eb4ea15421e2685d
CORELXD <corelxd53@gmail.com>
2023-10-22 12:03:49 +0700

    Laporan 19-21

---
+++ frontend01/src/pages/Jurusan.js
@@ -0,0 +1,14 @@
+import {  Container, Row, Col } from 'react-bootstrap';
+
+function Jurusan(){
+    return (
+        <Container>
+            <Row>
+                <Col><h2> DATA  JURUSAN</h2></Col>
+            </Row>
+
+        </Container>
+    );
+}
+
+export default Jurusan;
