2ee5789d6411438f7e3fbd86346392bf677e8872
CORELXD <corelxd53@gmail.com>
2023-11-10 11:39:07 +0700

    frontend/Styling

--- frontend01/src/template/Routing.js
+++ frontend01/src/template/Routing.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import '../Style.css';
 import Mahasiswa from '../pages/Mahasiswa';
 import Jurusan from '../pages/Jurusan';
 import Login from '../pages/auth/Login';
@@ -17,33 +19,35 @@ const handleLogout = () => {
     return (
         <Router>
         <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <a className="navbar-brand" href="#">Frontend</a>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <nav className="navbar navbar-expand-lg navbar-light" style={{ background: '#fff' }}>
+    <div className="container-fluid">
+    <Link className="navbar-brand" to="/" style={{ color: '#333', marginLeft: '20px' }}>Web Santai</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
             <ul className="navbar-nav">
                 <li className="nav-item">
-                <Link className="nav-link active" to="/mhs">Mahasiswa</Link>
+                    <Link className="nav-link" to="/mhs" style={{ color: '#333' }}>Mahasiswa</Link>
                 </li>
                 <li className="nav-item">
-                <Link className="nav-link" to="/jrsn">Jurusan</Link>
+                    <Link className="nav-link" to="/jrsn" style={{ color: '#333' }}>Jurusan</Link>
                 </li>
                 {isLoggedin ? (
-              <li className="nav-item">
-              <Link className="nav-link" onClick={handleLogout}>Logout</Link>
-              </li>
-              ) : (
-              <li className="nav-item">
-              <Link className="nav-link" to="/login">Login</Link>
-            </li>
-                 )}
+                    <li className="nav-item">
+                        <Link className="nav-link" onClick={handleLogout} style={{ color: '#333' }}>Logout</Link>
+                    </li>
+                ) : (
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/login" style={{ color: '#333' }}>Login</Link>
+                    </li>
+                )}
             </ul>
-            </div>
         </div>
-        </nav>
+    </div>
+</nav>
+
+
 
 
         <Routes>
